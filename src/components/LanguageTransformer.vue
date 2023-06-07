b91ee9d68507a2d2997f85ff651451709427f2a4
xiefei <xie0729@yeah.net>
2023-06-07 17:36:29 +0800

    feat:完成英语翻译

--- src/components/LanguageTransformer.vue
+++ src/components/LanguageTransformer.vue
@@ -1,7 +1,11 @@
-<template>
-  <div>languagetransformer</div>
-</template>
-
 <script setup lang="ts"></script>
+<template>
+  <iframe
+    wh-full
+    name="translator"
+    src="https://cn.bing.com/translator?ref=TThis&text=&from=zh-Hans&to=en"
+    frameborder="0"
+  ></iframe>
+</template>
 
 <style scoped></style>
